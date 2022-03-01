1b47520ed7793f7187db4bc33a76785ca28c19f2
KSantosh24 <73108688+KSantosh24@users.noreply.github.com>
2022-03-01 05:39:22 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyB4QBwRUJVcJJgZlPu9fjNtLjYz8osLsgo",
+    authDomain: "e-rider-79a8b.firebaseapp.com",
+    projectId: "e-rider-79a8b",
+    storageBucket: "e-rider-79a8b.appspot.com",
+    messagingSenderId: "706333916545",
+    appId: "1:706333916545:web:12f30f1ee0d5b7a2bcfc80"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
